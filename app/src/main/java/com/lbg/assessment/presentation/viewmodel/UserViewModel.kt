68d758b579f5f1e933a05905ff2cbc7a360e8f69
santoshkumar <santu758.msb@gmail.com>
2025-03-01 21:53:33 +0530

    Implemented presentation layer logic. viewmodel which holds non ui logic is the communication between domain and view. User Screen is the jet pack compose view which observes users data from the view model.

---
+++ app/src/main/java/com/lbg/assessment/presentation/viewmodel/UserViewModel.kt
@@ -0,0 +1,43 @@
+package com.lbg.assessment.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lbg.assessment.domain.model.User
+import com.lbg.assessment.domain.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UserViewModel @Inject constructor(private val userRepository: UserRepository) : ViewModel() {
+
+    private val _users = MutableStateFlow<List<User>>(emptyList())
+    val users: StateFlow<List<User>> = _users
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    private val _error = MutableStateFlow<String?>(null)
+    val errorMessage: StateFlow<String?> = _error
+
+    init {
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        viewModelScope.launch {
+            try {
+                _isLoading.value = true
+                _users.value = userRepository.fetchUsers()
+                _error.value = null
+            } catch (e: Exception) {
+                println("Error fetching users: ${e.message}")
+                _error.value = e.message
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
+}
